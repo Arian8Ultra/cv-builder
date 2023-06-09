53f88838d1c481fdf9141ad80f85e6e5effd4bdb
Arian Rezaei <87231161+Arian8Ultra@users.noreply.github.com>
2023-06-09 20:27:34 +0330

    new scripts

--- src/App/Home/Slide.tsx
+++ src/App/Home/Slide.tsx
@@ -1,17 +1,22 @@
 import { Box } from "@mui/material";
+import React from "react";
+import { checkVisible } from "../../functions/ui";
 
 interface SlideProps {
   children?: React.ReactNode;
   id?: string;
   className?: string;
   style?: React.CSSProperties;
+  zIndex?: number;
 }
 
 const Slide = (props: SlideProps) => {
+  const ref = React.useRef<HTMLDivElement>(null);
 
   return (
     <Box
       id={props.id}
+      ref={ref}
       className={props.className}
       style={props.style}
       width={1}
@@ -19,6 +24,7 @@ const Slide = (props: SlideProps) => {
       display={"flex"}
       justifyContent={"center"}
       alignItems={"center"}
+      zIndex={props.zIndex}
       sx={{
         scrollSnapAlign: "start",
       }}
